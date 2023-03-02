78a9056d3142e7adb455dac86178be1f4e7ba76c
Samuel <opeadeshina@hotmail.com>
2023-03-02 04:15:41 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 /**
- * main - A  c program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - A program that prints a lone to the standard error
+ * Return: Success (1)
  */
 int main(void)
 {
-char("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char msg[] = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, msg, sizeof(msg) - 1);
 return (1);
 }
